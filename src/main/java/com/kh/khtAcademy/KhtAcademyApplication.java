9f7809dcbb606d6dff71354af4fbd77575a1cbfa
Kyungnan Kim <kht241022@gmail.com>
2025-01-07 10:51:13 +0900

    25-01-07 ajax 활용한 login

--- src/main/java/com/kh/khtAcademy/KhtAcademyApplication.java
+++ src/main/java/com/kh/khtAcademy/KhtAcademyApplication.java
@@ -1,9 +1,11 @@
 package com.kh.khtAcademy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.kh.khtAcademy.mapper")
 public class KhtAcademyApplication {
 
 	public static void main(String[] args) {
