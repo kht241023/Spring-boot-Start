a9cfdcddeac0e2cc903f95d808ed1892c5058c97
Kyungnan Kim <kht241022@gmail.com>
2025-01-07 11:45:35 +0900

    25-01-07 error 직렬화

--- src/main/java/com/kh/khtAcademy/dto/User.java
+++ src/main/java/com/kh/khtAcademy/dto/User.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Id;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @AllArgsConstructor    // 필수 생성자
@@ -13,8 +14,24 @@ import java.util.Date;
 @Setter                // setter 줄임말로 사용
 @Getter                // getter 줄임말로 사용
 @ToString             //toString 줄임말로 사용
-public class User {
+public class User implements Serializable {
+    //  implements Serializable User에 저장된 객체를 파일이나 네트워크로 전송
+    //  객체를 임시저장 빠르게 복원
+    /*
+    * private static final long serialVersionUID = 1L 작성하지 않아도
+    * 프로그램 실행할 때마다 임의의 ID값이 설정
+    * 자동으로 생성된 ID값에 따라 저장
+    *
+    * 1 = 개발자가 설정하는 숫자값으로 무조건 1이어야할 이유 XXXXX
+    * Long 타입으로 버전 숫자를 작성
+    * */
+    private static final long serialVersionUID = 1L;
 
+
+
+    // 네트워크 전송을 위한 직렬화 버전
+    // 버전을 통해 객체를 버전별로 저장하고 불러옴 가능
+    // 보안 노출 위험
     @Id //primary key 표기로 id는 맨 위에 작성
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment 숫자자동증가임을 설정
     private int userId;
